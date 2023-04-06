b52e30ea47335d43ca196b6d3d59cc243696e55a
melizondo18 <melizondo10758@ufide.ac.cr>
2023-04-06 14:39:32 -0600

    Clase ColaJug + NodoColaJug
    Se agrega la cola ColaJug, en ella dos metodos, 1 para agregar la topa dependiendo de la ronda actual y otro que muestra la cantidad de tropas almacenadas en la cola

--- Proyecto_Tower_Defense/src/proyecto_tower_defense/ColaJug.java
+++ Proyecto_Tower_Defense/src/proyecto_tower_defense/ColaJug.java
@@ -2,5 +2,47 @@
 package proyecto_tower_defense;
 
 public class ColaJug {
+    private NodoColaJug cabeza;
+    private NodoColaJug cola;
+    private int qtyTropa;
     
-}
+    public ColaJug() {
+            this.cabeza = null;
+            this.cola = null;
+            this.qtyTropa = 0;
+        }
+     
+    // Metodo para agregar las tropas
+    public void agregaTropa(int ronda){ // Recibe la ronda actual para calcular
+        // la cantidad de tropas a elegir en esta ronda
+        int qtyTropa = ronda + 4;
+        NodoColaJug nuevoNodoColaJug = new NodoColaJug();
+        nuevoNodoColaJug.qtyTropa = qtyTropa;
+        nuevoNodoColaJug.setSiguiente; = null; // Agrega la cantidad de tropas
+        // a la cola
+          
+        if (cola == null){  // Verifica que la cabeza este vacia   
+            cabeza = nuevoNodoColaJug;   // cabeza y cola son el mismo nodo
+            cola = nuevoNodoColaJug;
+        } 
+        else{
+            cola.setSiguiente(nuevoNodoColaJug); //atrás del último es el nuevo nodo
+            cola = nuevoNodoColaJug;   // nuevo nodo es nuevo atrás
+        }
+    }
+    
+    // Metodo para obtener la tropa
+    public int getTropa(){ 
+        if (cabeza == null) {
+            System.out.println("Seleccione una tropa");
+        }
+        
+        int qtyTropa = cabeza.qtyTropa; // Devuelve la cantidad de tropas en la
+        cabeza = cabeza.getsiguiente(); // cabeza y se mueve al siguiente nodo
+        if (cabeza == null) {
+            cola = null;
+        }
+        return qtyTropa;
+    }
+    }
+
--- Proyecto_Tower_Defense/src/proyecto_tower_defense/NodoColaJug.java
+++ Proyecto_Tower_Defense/src/proyecto_tower_defense/NodoColaJug.java
@@ -3,5 +3,28 @@
 package proyecto_tower_defense;
 
 public class NodoColaJug {
-    
+        
+    private NodoColaJug siguiente;
+    int qtyTropa;
+
+    public NodoColaJug(NodoColaJug atras, int qtyTropa) {
+        this.siguiente = siguiente;
+        this.qtyTropa = qtyTropa;
+    }
+
+    public NodoColaJug getsiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(NodoColaJug atras) {
+        this.siguiente = siguiente;
+    }
+
+    public int getQtyTropa() {
+        return qtyTropa;
+    }
+
+    public void setQtyTropa(int qtyTropa) {
+        this.qtyTropa = qtyTropa;
+    }
 }
