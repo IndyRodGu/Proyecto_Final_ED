528c1b26c3af2cc61c176ec3a3e5ae1e6f6c250c
jennygong98 <114629733+jennygong98@users.noreply.github.com>
2023-04-13 16:12:47 -0600

    Cronometro (planB)
    eliminar cod de jugadores aleatorio del CPU

--- Proyecto_Tower_Defense/src/proyecto_tower_defense/Cronometro.java
+++ Proyecto_Tower_Defense/src/proyecto_tower_defense/Cronometro.java
@@ -36,3 +36,33 @@ public class Cronometro {
         }
     }
 }
+//
+//        long t1, t2, dif;
+//        String cad;
+//        Scanner teclado = new Scanner(System.in);
+//
+//        // Se toma la hora en el inicio del programa
+//        Calendar ahora1 = Calendar.getInstance(); // Crear una instancia de la clase Calendar
+//        t1 = ahora1.getTimeInMillis(); // Esta variable guarda la hora actual en milisegundos
+//        
+//        // Código de lo que se quiere cronometrar
+//        System.out.println("Empieza a contar el tiempo.");
+//        System.out.println("Pulsa Intro para terminar");
+//
+//        // Se espera q el usuario pulse Intro
+//        cad = teclado.nextLine();
+//        
+//        // Luego del código se obtiene la nueva hora en ms para posteriormente calcular la diferencia.
+//        //Se vuelve a tomar la hora una vez que ha pulsado Intro
+//        Calendar ahora2 = Calendar.getInstance();
+//        t2 = ahora2.getTimeInMillis();
+//
+//        /* El tiempo tardado será la diferencia de la hora tomada al final (t2)
+//         y la tomada al principio (t1), que se guardará en una variable de 
+//         tipo long. */
+//        dif = t2 - t1;
+//        
+//        // Se muestra en pantalla la diferencia de tiempo obtenido
+//        System.out.println("Has tardado: " + dif + "milisegundos");
+//        System.out.printf("Equivale a: %.3f segundos", (double) dif / 1000);
+//        
