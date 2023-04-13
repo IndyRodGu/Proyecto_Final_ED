43cb8dd5544678ba61d75032dc9ee7fefd2f4689
melizondo18 <melizondo10758@ufide.ac.cr>
2023-04-13 14:55:26 -0600

    Clase Cronometro
    Se agrrega el cronometro

--- Proyecto_Tower_Defense/src/proyecto_tower_defense/Proyecto_Tower_Defense.java
+++ Proyecto_Tower_Defense/src/proyecto_tower_defense/Proyecto_Tower_Defense.java
@@ -31,7 +31,7 @@ public class Proyecto_Tower_Defense {
                     break;
 
                 case (2): // Termina 
-                    juego = false;
+                    juego = false;                    
                     break;
 
                 default:
