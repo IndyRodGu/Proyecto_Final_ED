feab1ab297d3a59e459259a69e1603a725eaafe1
melizondo18 <melizondo10758@ufide.ac.cr>
2023-04-18 11:57:34 -0400

    Correcciones
    Se realizan correcciones de errores y eliminacion de codigo no utilizado

--- Proyecto_Tower_Defense/src/proyecto_tower_defense/TipoTropa.java
+++ Proyecto_Tower_Defense/src/proyecto_tower_defense/TipoTropa.java
@@ -2,10 +2,4 @@ package proyecto_tower_defense;
 
 public enum TipoTropa {
     MAGO,CABALLERO,ARQUERO;
-
-    char[] toCharArray() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-
 }
